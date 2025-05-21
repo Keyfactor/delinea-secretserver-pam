46442010484fcf83f64570848a2285d023d06992
spbsoluble <1661003+spbsoluble@users.noreply.github.com>
2025-05-21 10:55:16 -0700

    feat(client): Add support for `client_credentials` grant type.

--- delinea-secretserver-pam/Models/SecretResponse.cs
+++ delinea-secretserver-pam/Models/SecretResponse.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2023 Keyfactor
+﻿// Copyright 2025 Keyfactor
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 // Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
@@ -22,6 +22,6 @@ namespace Keyfactor.Extensions.Pam.Delinea.Models
 
         [JsonProperty("active")] public bool Active { get; set; }
 
-        [JsonProperty("items")] public List<DelineaSecret> Items { get; set; }
+        [JsonProperty("items")] public List<DelineaSecret> Items { get; set; } = new List<DelineaSecret>();
     }
 }
